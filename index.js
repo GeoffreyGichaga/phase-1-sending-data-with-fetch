c043021924ea150c7061a77633974b94b8372e60
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-24 13:38:55 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(name,email)
+{
+    fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+            Accept:'application/json',
+        },
+        body:JSON.stringify({name,email}),
+
+    })
+    .then(res=>res.json())
+
+    .then((data)=>{
+
+        const h2 = document.querySelector('#name')
+        h2.innerHTML = data.id
+
+
+    })
+   .catch((error)=>{
+    const par = document.querySelector('#message')
+    par.innerHTML = error
+   })
+}
+
